a56b39800ff13f47452b5dd099ac403542553c99
Goran Tolušić <121817504+GoranTolusic@users.noreply.github.com>
2024-03-16 18:01:19 +0100

    added apache

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y git
 sudo apt-get install -y git mysql-client
 sudo apt-get install -y python3 python3-pip
 sudo apt-get install -y php php-cli php-mysql
+sudo apt-get install apache2
 
 # Install visual studio code
 sudo snap install --classic code
