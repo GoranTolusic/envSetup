537edb8527829f5e5044120d8870cd4f2cf862fd
Goran Tolušić <121817504+GoranTolusic@users.noreply.github.com>
2024-03-16 18:13:06 +0100

    update pg admin setup

--- setup.sh
+++ setup.sh
@@ -22,7 +22,9 @@ nvm install 16.14.0
 nvm use 16.14.0
 
 # Pg admin
-sudo apt-get install -y pgadmin4
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4
 
 # Postman
 sudo snap install postman
