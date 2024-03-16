38cbdc78fb7774a934848b9fe93bd296486081fd
Goran Tolušić <121817504+GoranTolusic@users.noreply.github.com>
2024-03-16 18:29:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,6 +34,9 @@ sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 sudo apt install pgadmin4
 
+#Dbeaver
+sudo snap install dbeaver-ce
+
 # Postman
 sudo snap install postman
 
